fe6f8e0c88dff57b0edcd2bbd1f50f152a8228c1
MarioR17 <jmrendonq17@gmail.com>
2024-05-17 18:08:09 -0400

    Created making and deletion of hashtable and items implementation

---
+++ src/hashtable.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <hashtable.h>
+
+static htItem * htNewItem(const char * k, const char * v) {
+    htItem * i = malloc(sizeof(htItem));
+    i->key = strdup(k);
+    i->value = strdup(v);
+
+    return i;
+}
+
+htHashTable * htNew() {
+    htHashtable * ht = malloc(sizeof(htHashTable));
+
+    ht->size = 53; 
+    ht->count = 0;
+    ht->items = calloc((size_t) ht->size, sizeof(htItem *));
+
+    return ht;
+}
+
+static void htDelItem(htItem * i) {
+    free(i->key);
+    free(i->value);
+    free(i);
+}
+
+void htDelHashtable(htHashTable * ht) {
+    for (int i = 0; i < ht->size; i++) {
+        htItem * item = ht->items[i]; 
+        if (item != NULL) {
+            htDelItem(item);
+        }
+    }
+
+    free(ht->items);
+    free(ht);
+}
+
+
+
